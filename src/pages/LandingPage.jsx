fdbc00482bf2fe5c98c3532f310292789d822f73
hyeongyong-choi <guddyd6761@naver.com>
2022-08-06 20:03:42 +0900

    commit

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,15 @@
 import Button from "components/Button";
+import LandingTodolist from "components/LandingTodolist";
 
 const LandingPage = () => {
   return (
     <div>
-      <h1>LandingPage</h1>
+      <h1>무엇을 할까요?</h1>
+      <LandingTodolist />
     </div>
   );
 };
 
 export default LandingPage;
+
+
--- src/pages/TodoFormPage.jsx
+++ src/pages/TodoFormPage.jsx
@@ -1,7 +1,14 @@
+import React from "react";
+import Input from "components/Input";
+
 const TodoFormPage = () => {
   return (
     <div>
       <h1>TodoFormPage</h1>
+      <h3>작성자</h3>
+      <Input placeholder='작성자의 이름을 입력해주세요(5자 이내)'></Input>
+      <h3>제목</h3>
+      <Input placeholder='제목을 입력해주세요(50자 이내)'></Input>
     </div>
   );
 };

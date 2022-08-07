201085131ac14c0bb9c81d9edf7f3d4e6f4f7274
GEUNA <geuna0204@gmail.com>
2022-08-07 16:13:22 +0900

    [fix] TodoFormPage 컴포넌트 경로 수정

--- src/pages/TodoFormPage.jsx
+++ src/pages/TodoFormPage.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import Input from "components/Input";
-import Header from 'components/Header'
-import Button from "components/Button";
-
+import React from 'react';
+import Header from 'components/Header';
+import Button from 'components/elements/Button';
+import Input from 'components/elements/Input';
 
 const TodoFormPage = () => {
   return (
     <div>
-      <Header/>
+      <Header />
       <h1>TodoFormPage</h1>
       <h3>작성자</h3>
-      <Input placeholder='작성자의 이름을 입력해주세요(5자 이내)'></Input>
+      <Input placeholder="작성자의 이름을 입력해주세요(5자 이내)"></Input>
       <h3>제목</h3>
-      <Input placeholder='제목을 입력해주세요(50자 이내)'></Input>
-      <Button size='large'>추가하기</Button>
+      <Input placeholder="제목을 입력해주세요(50자 이내)"></Input>
+      <Button size="large">추가하기</Button>
     </div>
   );
 };

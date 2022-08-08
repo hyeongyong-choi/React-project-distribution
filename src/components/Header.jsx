e28e6a4df96dd0d5d0e11886f6fc5cb10c977b42
GEUNA <geuna0204@gmail.com>
2022-08-08 15:05:19 +0900

    [fix] Header 컴포넌트 router 연결

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from './elements/Button';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const onClickHandler = () => navigate('/');
+
   return (
     <HeaderContainer>
-      <Button variant="home"/>
+      <Button variant="home" clickHandler={onClickHandler} />
       <h1>모두의 투두리스트</h1>
     </HeaderContainer>
   );
